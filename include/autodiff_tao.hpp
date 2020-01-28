c9bb79c58397c61d09a88c034911d7364a276a6b
michael <mfdeakin@cs.unc.edu>
2020-01-27 16:08:46 -0800

    Fix reliance on undefined behavior for the template parameter pack deduction. Finish constructor for the nonlinear optimizer

--- include/autodiff_tao.hpp
+++ include/autodiff_tao.hpp
@@ -15,70 +15,64 @@
 namespace auto_diff {
 namespace optimize_tao {
 
-class PetscException {
-public:
-  PetscException(const char *err_msg_, const PetscErrorCode err_code_)
-      : err_msg(err_msg_), err_code(err_code_) {}
+// This is to put the petsc_cons type in a non-deducable context; otherwise this
+// code relies on undefined behavior since petsc_cons cannot be used to deduce
+// the types of constructor_args (as it's not the last set of parameters of the
+// function; attempting to in this case is undefined behavior)
+// Note that the standard type_identity_t should be used when switching to C++20
+template <typename type_> struct type_identity { using type = type_; };
+template <typename type_>
+using type_identity_t = typename type_identity<type_>::type;
 
-  const char *err_msg;
-  PetscErrorCode err_code;
-};
+template <typename obj_type, typename... constructor_args>
+using petsc_cons = PetscErrorCode (*)(constructor_args..., obj_type *);
 
-template <typename param, typename... constructor_args>
-using petsc_cons = PetscErrorCode (*)(constructor_args..., param *);
+template <typename obj_type> using petsc_destr = PetscErrorCode (*)(obj_type *);
 
-template <typename param> using petsc_destr = PetscErrorCode (*)(param *);
-
-template <typename param, typename... constructor_args>
-auto petsc_make_unique(petsc_cons<param, constructor_args...> construct,
-                       petsc_destr<param> destroy, constructor_args... args) {
-  param p;
+// Wraps petsc types in unique pointers for RAII
+template <typename obj_type, typename... constructor_args>
+auto petsc_make_unique(
+    type_identity_t<petsc_cons<obj_type, constructor_args...>> construct,
+    petsc_destr<obj_type> destroy, constructor_args... args) {
+  obj_type p;
   const PetscErrorCode err = construct(args..., &p);
   if (err) {
-    throw PetscException("Error constructing petsc object", err);
+    throw std::runtime_error("Error constructing petsc object");
   }
-  return std::unique_ptr<typename std::remove_pointer_t<param>,
-                         std::function<void(param)>>(
-      p, [destroy](param d) { // No exception check here, because it's unclear
-                              // that anything can be done
-        destroy(&d);
-      });
+  auto destroy_functor = [destroy](obj_type d) {
+    PetscErrorCode err = destroy(&d);
+    if (err) {
+      // Presumably this never happens...
+      throw std::runtime_error("Error destroying petsc object");
+    }
+  };
+  return std::unique_ptr<typename std::remove_pointer_t<obj_type>,
+                         std::function<void(obj_type)>>(p, destroy_functor);
 }
 
 template <typename expr_t> class NLOptimizer {
 public:
   NLOptimizer(const expr_t f_, const size_t num_vars)
-      : f(f_), tao_ctx(petsc_make_unique<Tao>(
-                   static_cast<petsc_cons<Tao, MPI_Comm>>(TaoCreate),
-                   static_cast<petsc_destr<Tao>>(TaoDestroy),
-                   PETSC_COMM_SELF)) // ,
-  // arg_min(petsc_make_unique<Vec, MPI_Comm, PetscInt>(
-  //     VecCreateSeq, VecDestroy, PETSC_COMM_SELF, num_vars))
-  {
-    // PetscErrorCode err = TaoCreate(PETSC_COMM_SELF, &t);
-    // tao_ctx = decltype(tao_ctx)(t, TaoDestructor);
-    // err |= TaoSetType(tao_ctx, TAOCG);
-    // if (err) {
-    //   throw PetscException(
-    //       "Failed to create Tao Context or set the Tao optimization method");
-    // }
+      : f(f_), tao_ctx(petsc_make_unique<Tao, MPI_Comm>(&TaoCreate, &TaoDestroy,
+                                                        PETSC_COMM_SELF)),
+        arg_min(petsc_make_unique<Vec, MPI_Comm, PetscInt>(
+            VecCreateSeq, VecDestroy, PETSC_COMM_SELF, num_vars)),
+        hessian(petsc_make_unique<Mat, MPI_Comm, int, int, void *>(
+            MatCreateSeqDense, MatDestroy, PETSC_COMM_SELF, num_vars, num_vars,
+            nullptr)) {
+    PetscErrorCode err = TaoSetType(tao_ctx.get(), TAOCG);
+    if (err) {
+      throw std::runtime_error(
+          "Failed to create Tao Context or set the Tao optimization method");
+    }
 
     std::set<id_t> vars;
     assert(f_.expr_vars(vars).size() == num_vars);
 
-    // Vec v;
-    // err = VecCreateSeq(PETSC_COMM_SELF, num_vars, &v);
-    // arg_min = decltype(arg_min)(v, VecDestructor);
-    // err |= VecSet(arg_min, 0.0);
-    // if (err) {
-    //   throw PetscException("Failed to create or set the vector");
-    // }
-    // err =
-    //     MatCreateSeqDense(MPI_COMM_SELF, num_vars, num_vars, nullptr,
-    //     &hessian);
-    // if (err) {
-    //   throw PetscException("Failed to create hessian");
-    // }
+    err = VecSet(arg_min.get(), 0.0);
+    if (err) {
+      throw std::runtime_error("Failed to create or set the vector");
+    }
   }
 
 protected:
